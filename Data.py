a916e1e134a8b744efc79e1ddba58cc01bc69e1e
FrankensteinMazino <153411558+FrankensteinMazino@users.noreply.github.com>
2023-12-12 20:07:25 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -6,24 +6,24 @@ from pyrogram.types import InlineKeyboardButton
 
 class Data:
     HELP = """
-<b> ❏ Perintah untuk Pengguna BOT
+<b> ❏ Perintah Untuk Pengguna BOT
  ├ /start - Mulai Bot
  ├ /about - Tentang Bot ini
  ├ /help - Bantuan Perintah Bot ini
- ├ /ping - Untuk mengecek bot hidup
- └ /uptime - Untuk melihat status bot 
+ ├ /ping - Untuk Mengecek Bot Hidup
+ └ /uptime - Untuk Melihat Status Bot 
  
  ❏ Perintah Untuk Admin BOT
- ├ /logs - Untuk melihat logs bot
- ├ /setvar - Untuk mengatur var dengan command dibot
- ├ /delvar - Untuk menghapus var dengan command dibot
- ├ /getvar - Untuk melihat salah satu var dengan command dibot
- ├ /users - Untuk melihat statistik pengguna bot
- ├ /batch - Untuk membuat link lebih dari satu file
- ├ /speedtest - Untuk Mengetes kecepatan server bot
- └ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
-
-👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
+ ├ /logs - Untuk Melihat Logs Bot
+ ├ /setvar - Untuk Mengatur Var Dengan Command Dibot
+ ├ /delvar - Untuk Menghapus Var Dengan Command Dibot
+ ├ /getvar - Untuk Melihat Salah Satu Var Dengan Command Dibot
+ ├ /users - Untuk Melihat Statistik Pengguna Bot
+ ├ /batch - Untuk Membuat Link Lebih Dari Satu File
+ ├ /speedtest - Untuk Mengetes Kecepatan Server Bot
+ └ /broadcast - Untuk Mengirim Pesan Broadcast ke Pengguna Bot
+ 
+👨‍💻 Develoved by </b><a href='https://t.me/faksyiit'>@faksyiit</a>
 """
 
     close = [
@@ -51,7 +51,7 @@ class Data:
 
  • Creator: @{}
  • Framework: <a href='https://docs.pyrogram.org'>Pyrogram</a>
- • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>File-Sharing-Man v4</a>
+ • Programming Language: <a href='https://www.python.org'>Python</a>
 
-👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
+👨‍💻 Develoved by </b><a href='https://t.me/faksyiit'>@faksyiit</a>
 """
