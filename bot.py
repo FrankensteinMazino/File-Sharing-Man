fae346c677044956f5972ea5e4de407549bcb869
FrankensteinMazino <153411558+FrankensteinMazino@users.noreply.github.com>
2024-01-03 06:28:26 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ class Bot(Client):
         except Exception as a:
             self.LOGGER(__name__).warning(a)
             self.LOGGER(__name__).info(
-                "Bot Berhenti. Coba Tanya https://t.me/faksyiit untuk Bantuan"
+                "Bot Berhenti. Coba Tanya ke https://t.me/callmesunoo untuk meminta Bantuan"
             )
             sys.exit()
 
@@ -68,7 +68,7 @@ class Bot(Client):
                     f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "Bot Berhenti. Coba Tanya https://t.me/faksyiit untuk Bantuan"
+                    "Bot Berhenti. Coba Tanya ke https://t.me/callmesunoo untuk meminta Bantuan"
                 )
                 sys.exit()
 
@@ -92,7 +92,7 @@ class Bot(Client):
                     f"Pastikan @{self.username} adalah admin di Group Tersebut, Chat ID F-Subs Group Saat Ini: {FORCE_SUB_GROUP}"
                 )
                 self.LOGGER(__name__).info(
-                    "Bot Berhenti. Coba Tanya https://t.me/faksyiit untuk Bantuan"
+                    "Bot Berhenti. Coba Tanya ke https://t.me/callmesunoo untuk meminta Bantuan"
                 )
                 sys.exit()
 
@@ -110,13 +110,13 @@ class Bot(Client):
                 f"Pastikan @{self.username} adalah admin di Channel DataBase anda, CHANNEL_ID Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "Bot Berhenti. Coba Tanya https://t.me/faksyiit untuk Bantuan"
+                "Bot Berhenti. Coba Tanya ke https://t.me/callmesunoo untuk meminta Bantuan"
             )
             sys.exit()
 
         self.set_parse_mode(enums.ParseMode.HTML)
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan https://t.me/faksyiit"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/callmesunoo"
         )
 
     async def stop(self, *args):
