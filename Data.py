6b897e34d26b452a4442a280fe7a1b388c05166d
FrankensteinMazino <153411558+FrankensteinMazino@users.noreply.github.com>
2024-01-03 06:44:34 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,7 +1,3 @@
-# Credits: @mrismanaziz
-# FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
-# t.me/SharingUserbot & t.me/Lunatic0de
-
 from pyrogram.types import InlineKeyboardButton
 
 class Data:
